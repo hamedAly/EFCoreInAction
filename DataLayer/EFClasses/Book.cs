2b49b3b729e229e28b1fa8a6ee0e26578d16ac87
hamed aly <7amed.aly@gmail.com>
2022-12-04 14:07:11 +0200

    Add project files.

---
+++ DataLayer/EFClasses/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.EFClasses
+{
+    internal class Book
+    {
+        public int BookId { get; set; }
+        public string  Name { get; set; }
+    }
+}
